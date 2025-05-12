a1fcee93a3ab3ee313fad654356ef865b53121dd
Marco Angermann <159908924+MarcoAngermann@users.noreply.github.com>
2025-05-12 10:44:59 +0200

    videoplayer routing / mobile view

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { MainpageComponent } from "./features/mainpage/mainpage.component";
 import { RegisterComponent } from "./features/auth/register/register.component";
 import { ImprintComponent } from "./shared/components/imprint/imprint.component";
 import { PrivacyPolicyComponent } from "./shared/components/privacy-policy/privacy-policy.component";
+import { PreviewComponent } from "./features/preview/preview.component";
+import { VideoPlayerComponent } from "./features/video-player/video-player.component";
 
 export const routes: Routes = [
 	{
@@ -17,6 +19,14 @@ export const routes: Routes = [
 	{
 		path: "mainpage",
 		component: MainpageComponent,
+	},
+	{
+		path: "preview",
+		component: PreviewComponent,
+	},
+	{
+		path: "video-player",
+		component: VideoPlayerComponent,
 	},
 	{
 		path: "imprint",
